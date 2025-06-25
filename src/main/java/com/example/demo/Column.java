37d3366989c0db34d3010add6cccf0e7a0bbc399
aleandivanov <aleandivanov@phoenixit.ru>
2025-06-25 11:57:41 +0300

    add spring boot project

---
+++ src/main/java/com/example/demo/Column.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public interface Column {
+    String getName();
+    String getDataType();
+    String getComment();
+    String isMandatory();
+    String getDefautValue();
+}
