78bdbd00df538fe4e861802616be2f8c781c3b8a
aleandivanov <aleandivanov@phoenixit.ru>
2025-07-10 15:53:17 +0300

    Реализовал создание SQLGenerator и убрал лишние конфиги

--- src/main/java/sqlgen/SQLGeneratorCreator.java
+++ src/main/java/sqlgen/SQLGeneratorCreator.java
@@ -3,7 +3,13 @@ package sqlgen;
 import sqlgen.entity.SQLGenerator;
 
 public class SQLGeneratorCreator {
-    public static SQLGenerator create(GeneratorConfig) {
-
+    public static SQLGenerator create(AppConfig appConfig) {
+        String typeGeneration = appConfig.getCurrentTypeGeneration();
+        return switch (typeGeneration) {
+            case "ClickHouse" -> new ClickHouseGenerator(appConfig);
+            case "GreenPlum" -> new GreenPlumGenerator(appConfig);
+            case "PostgreSQL" -> new PostgreSQLGenerator(appConfig);
+            default -> new EmptyGenerator(appConfig);
+        };
     }
 }
--- src/main/java/sqlgen/SqlGeneratorApplication.java
+++ src/main/java/sqlgen/SqlGeneratorApplication.java
@@ -13,14 +13,10 @@ public class SqlGeneratorApplication {
     
     public static void run() {
         AppConfig appConfig = new AppConfig(CONFIG_PATH);
-        DWHConfig dwhConfig = appConfig.getDWHConfig();
-        ParserConfig parserConfig = appConfig.getParserConfig();
-        GeneratorConfig generatorConfig = appConfig.getGeneratorConfig();
 
-
-        SQLGenerator sqlGenerator = SQLGeneratorCreator.create(generatorConfig);
+        SQLGenerator sqlGenerator = SQLGeneratorCreator.create(appConfig);
         List<SQLFile> sqlFiles = sqlGenerator.generate();
         SQLWriter sqlWriter = new SQLWriter(sqlFiles);
-        sqlWriter.saveAs(appConfig.getDefault().getSaveFolder());
+        sqlWriter.saveAs(appConfig.getSaveFolder());
     }
 }
