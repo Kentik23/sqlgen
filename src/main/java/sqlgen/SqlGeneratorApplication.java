af5f3d71d876cbb279d70c78e22245e6a8002d18
aleandivanov <aleandivanov@phoenixit.ru>
2025-07-11 16:02:58 +0300

    Создал конфигурационный класс

--- src/main/java/sqlgen/SqlGeneratorApplication.java
+++ src/main/java/sqlgen/SqlGeneratorApplication.java
@@ -1,23 +1,14 @@
 package sqlgen;
 
 import sqlgen.config.AppConfig;
-import sqlgen.entity.SQLFile;
-import sqlgen.entity.SQLGenerator;
-import sqlgen.io.SQLWriter;
-
-import java.util.List;
 
 public class SqlGeneratorApplication {
-
-    private static final String APPLICATION_PATH = "";
-    private static final String CONFIG_PATH = APPLICATION_PATH + "/config/appConfig.yaml";
-    
     public static void run() {
-        AppConfig appConfig = new AppConfig(CONFIG_PATH);
-
-        SQLGenerator sqlGenerator = SQLGeneratorCreator.create(appConfig);
-        List<SQLFile> sqlFiles = sqlGenerator.generate();
-        SQLWriter sqlWriter = new SQLWriter(sqlFiles);
-        sqlWriter.saveAs(appConfig.getSaveFolder());
+        try {
+            AppConfig appConfig = new AppConfig();
+            System.out.println(appConfig.getProperty("test"));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
     }
 }
