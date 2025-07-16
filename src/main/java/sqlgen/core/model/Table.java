1a5d1d7c179f7f1aad5adc6896fe2521fb8321c6
aleandivanov <aleandivanov@phoenixit.ru>
2025-07-16 18:16:22 +0300

    Поменял код модели

--- src/main/java/sqlgen/core/model/Table.java
+++ src/main/java/sqlgen/core/model/Table.java
@@ -1,12 +1,51 @@
 package sqlgen.core.model;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-public class Table {
+public abstract class Table {
+    private Schema schema;
     private String code;
     private String comment;
     private List<Column> columns;
+
+    public Table(String code, String comment) {
+        this.code = code;
+        this.comment = comment;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+
+    public Schema getSchema() {
+        return schema;
+    }
+
+    public void setSchema(Schema schema) {
+        this.schema = schema;
+    }
+
+    public abstract String getCreateScript();
+
+    public abstract String getAddColumnsScript(List<Column> columns);
 }
