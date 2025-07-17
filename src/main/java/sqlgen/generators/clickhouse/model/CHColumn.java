c361372c1f72b46c82208c5b0ad707d0af52f8a4
aleandivanov <aleandivanov@phoenixit.ru>
2025-07-17 10:53:27 +0300

    Добавил модель кликхауса

---
+++ src/main/java/sqlgen/generators/clickhouse/model/CHColumn.java
@@ -0,0 +1,9 @@
+package sqlgen.generators.clickhouse.model;
+
+import sqlgen.core.model.Column;
+
+public class CHColumn extends Column {
+    public CHColumn(String code, String datatype, String defaultValue, String comment, boolean mandatory) {
+        super(code, datatype, defaultValue, comment, mandatory);
+    }
+}
